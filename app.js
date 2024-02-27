fbcc622fb3df05bea473f59af47dac5a4dc9758e
babayaga0 <baba_yaga0@mail.ru>
2024-02-27 18:03:32 +0300

    1_commit

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+const path = require("path");
+
+app.set("view engine", "ejs");
+app.use(express.static("/public"));
+
+
+app.get("/", (req, res) => {
+    res.render("index");
+});
+
+app.listen(port, ()=>{
+    console.log(`Твой сервак запущен тут http://localhost:${port}, только тихо))`)
+});
+
+
+app.get("/about", (req,res) =>{
+    res.send("О нас вы тут инфы не найдете")
+})
+
+app.get("/my_product", (req, res) => {
+    res.send("В процессе верстки")
+});
+
+app.get("/reg_auth", (req, res) => {
+    res.redirect("public/registration_auth.html")
+})
