9fa505dd03492ffed60f3a77f26185d9f1eb255b
babayaga0 <baba_yaga0@mail.ru>
2024-03-03 18:07:48 +0300

    dont ended reg

--- app.js
+++ app.js
@@ -2,10 +2,14 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const path = require("path");
+const mysql = require("mysql");
+var parseUrl = require('body-parser');
+const { Session } = require("inspector");
+
+let encodeUrl = parseUrl.urlencoded({ extended: false });
 
 app.set("view engine", "ejs");
 app.use(express.static('public'));
-
 
 app.get("/", (req, res) => {
     res.render("index");
@@ -27,3 +31,42 @@ app.get("/my_product", (req, res) => {
 app.get("/registration", (req, res) => {
     res.sendFile('C:\\Users\\baba_yaga0\\Desktop\\Alexandria_node\\public\\registration_auth.html')
 });
+
+app.get("/authorization", (req, res) => {
+    res.sendFile('C:\\Users\\baba_yaga0\\Desktop\\Alexandria_node\\public\\authorization.html')
+})
+
+
+// Ниже авторизация и регистрация и всё что к этому надо
+
+
+// подключение к бдшке
+const connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "Alexandria",
+});
+
+// Забор данных с формы
+app.post('/register', encodeUrl, (req, res) => {
+    var firstName = req.body.firstName;
+    var surName = req.body.surName;
+    var mail = req.body.mail;
+    var login = req.body.login;
+    var password = req.body.password;
+
+    
+});
+
+// midlleware сессии
+app.use(Session({
+    secret: "dontcarewhatwrotethis",
+    saveUninitialized: true,
+    cookie: {maxAge: 1000 * 60 * 60 * 12}, //12 часов
+    resave: false
+}));
+
+app.use(cookieParser()); // используется для разбора и составления HTTP-куки, позволяя Node.js работать с куками, отправленными клиентом
+
+
--- bd.js
+++ bd.js
@@ -1,24 +1,13 @@
-const mysql = require("mysql");
-const app = express();
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static("public"));
 
-const connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "Alexandria",
-});
+// const app = express();
 
-connection.connect({
-    if(err){
-        console.error("Ошибка соединения" + err.message);
-        return;
-    }
-});
+// app.use(express.static("public"));
 
-app.post("/register", (req, res) => {
-    const {username, secondname, mail,password, login } = req.body;
 
-    
-});
+
+// connection.connect({
+//     if(err){
+//         console.error("Ошибка соединения" + err.message);
+//         return;
+//     }
+// });
