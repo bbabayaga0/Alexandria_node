0e437f4f956ffcea405c4545274f5af9042b13b5
babayaga0 <baba_yaga0@mail.ru>
2024-03-12 14:56:58 +0300

    end reg_auth, middle admin_panel

--- app.js
+++ app.js
@@ -52,6 +52,10 @@ app.get("/personal_office", (req, res) => {
     res.sendFile('C:\\Users\\baba_yaga0\\Desktop\\Alexandria_node\\public\\personal_office.html')
 })
 
+app.get("/admin_panel", (req, res) => {
+    res.sendFile('C:\\Users\\baba_yaga0\\Desktop\\Alexandria_node\\public\\admin_panel.html')
+})
+
 // Ниже авторизация и регистрация и всё что к этому надо
 
 
@@ -114,15 +118,15 @@ app.post('/register', encodeUrl, (req, res) => {
                         };
                     });
             }
-        });//92 строка 
-    });// с 86 строки
-}); //80 cтрока
+        });//96 строка 
+    });// с 90 строки
+}); //84 cтрока
+
+
 
 
 //Авторизация юзера на сайте 
-
-
-// проверку и прочее в блок ниже как и с регом
+// проверка и прочее в блоке ниже как и с регистрацией
 app.post("/personal_office", encodeUrl, (req, res) => {
     // забор данных с формы при авторизации 
     var userLogin = req.body.login_from_user;
@@ -131,12 +135,12 @@ app.post("/personal_office", encodeUrl, (req, res) => {
     // проверка 
     connection.connect(function(err) {
         if(err){
-            console.log("Неправильные введёные данные")
+            document.writeln("Неправильные введёные данные")
         };
 
         connection.query(`SELECT * FROM users where login = '${userLogin}' AND password = '${userPassword}'`, function (err, result){
             if(err){
-                console.log("Такого юзера нету, досвидание")
+                res.sendFile('C:\\Users\\baba_yaga0\\Desktop\\Alexandria_node\\public\\FailAuth.html');
             };
             function userPage(){
                 // создание ссесии для юзера и хранение его инфы
@@ -151,12 +155,34 @@ app.post("/personal_office", encodeUrl, (req, res) => {
             if(Object.keys(result).length > 0){
                 userPage();
             }else{
-                console.log("Ошибка при авторизации, такого юзера не существует")
+                res.sendFile('C:\\Users\\baba_yaga0\\Desktop\\Alexandria_node\\public\\FailAuth.html');
             }
         });
     })
 });
 
+
+// обработка заказов из бд для отображения в админ-панели
+
+app.get('/orders_users', (req,res ) =>{
+    connection.connect(err => {
+        if(err){
+            return res.status(500).send({error: 'Ошибка подключения к базе данных'})
+        }
+
+        const query = `SELECT * FROM orders`;
+
+        connection.query(query, (err, results) => {
+            if(err){
+                console.log("ошибка выполения запроса")
+            }
+        
+            res.send({ orders: results });
+        });
+    })
+})
+
+// 
 
 /*
 
